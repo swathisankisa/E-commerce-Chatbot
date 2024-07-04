067c99bccc3a6cbdc4bafb807d6d208f999c8be3
Swathi Sankisa <54534818+swathisankisa@users.noreply.github.com>
2024-07-04 11:22:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="Ecommercebot",
     version="0.0.1",
-    author="sunny",
-    author_email="sunny.savita@ineuron.ai",
+    author="Swathi",
+    author_email="swatiapr30@gmail.com",
     packages=find_packages(),
     install_requires=['langchain-astradb','langchain ','langchain-openai','datasets','pypdf','python-dotenv','flask']
 )
